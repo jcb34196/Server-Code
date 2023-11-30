316887d539187250af7db955bb6c84fc569092bf
jcb34196 <119687284+jcb34196@users.noreply.github.com>
2023-11-29 19:05:27 -0500

    Uploading ServerCode

---
+++ Server_Code/app.js
@@ -0,0 +1,37 @@
+//const express = require('express');
+//const app = express();
+//app.get('/', (req, res) => res.send('Hello world!'));
+//const port = process.env.PORT || 8082;
+//app.listen(port, () => console.log(`Server running on port
+//${port}`));
+// setting up routes
+
+const express = require('express');
+const app =express();
+const port = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+const cors = require('cors');
+//const router = express.Router();
+//const bodyParser = require('body-parser')
+
+// connect database
+app.use(cors({origin: true, credentials:true}));
+app.use(express.json({extend: false}));
+app.get('/', (req, res) => res.send('Hello world!'));
+
+const conn_str = 'mongodb+srv://ProjData:s43UO2sKtjy1MHez@cluster0.lkpkoxu.mongodb.net/?retryWrites=true&w=majority'
+mongoose.set('strictQuery', false);
+mongoose.connect(conn_str)
+.then(()=> {
+    app.listen(port)
+    console.log(`MongoDB Connection Suceeded...`)
+})
+.catch(err => {
+    console.log(`Error in DB Connection ${err}`);
+});
+// exercise goes here
+//const items = require('./routes/api/items');
+//app.use('/api/items',items);
+
+const users = require('./routes/api/users');
+app.use('/api/users',users);
